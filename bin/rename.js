a8bd8e2c55331aba759afb40c345df76aad9cd36
Ismail El Korchi <ismail.elkorchi@gmail.com>
2020-04-18 16:37:37 +0100

    Read all the subdirectories recursively

--- bin/rename.js
+++ bin/rename.js
@@ -24,35 +24,56 @@ const excludes = [
 ///joining path of directory 
 const directoryPath = path.join(__dirname, '../');
 //passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        
-        if (err) { throw err; }
-        
-        if( ! excludes.includes(file)){
+function readdircontent(directoryPath) {
 
-            // Do whatever you want to do with the file
-            console.log(directoryPath + file); 
-            fs.readFile(file, 'utf8', function (err,data) {
-                if (err) {
-                  return console.log(err);
+    if ( ! fs.lstatSync(directoryPath).isFile()) { 
+
+        fs.readdir(directoryPath, function (err, files) {
+            //handling error
+            if (err) {
+                return console.log('Unable to scan directory: ' + err);
+            } 
+            
+            //listing all files using forEach
+            files.forEach(function (file) {
+
+            // console.log( file);
+                
+                if (err) { throw err; }
+
+                if (! excludes.includes(file)){
+
+                    if (fs.lstatSync(directoryPath + file).isDirectory()) { 
+                        console.log("//-------------------------- Sub Directory -----------------" + file);
+                        readdircontent(directoryPath + file+ '/');
+                    } else {
+                        DoReplacement(directoryPath + file);
+                    }
                 }
-                var result = data.replace(/'_s'/g, 'text-domain');
-                result = result.replace(/_s_/g, 'funcntion_name_');
-                result = result.replace(/Text Domain: _s/g, 'Text Domain: text-domain');
-                result = result.replace(/_s/g, 'Dock_block');
-                result = result.replace(/_s-/g, 'prefix-handler');
-                result = result.replace(/_S_/g, 'CONST');
-              
-                fs.writeFile(file, result, 'utf8', function (err) {
-                   if (err) return console.log(err);
-                });
             });
+        });
+    }
+}
+
+
+function DoReplacement(file) {
+    // Do whatever you want to do with the file
+    console.log(directoryPath + file); 
+    fs.readFile(file, 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
         }
+        var result = data.replace(/'_s'/g, 'text-domain');
+        result = result.replace(/_s_/g, 'funcntion_name_');
+        result = result.replace(/Text Domain: _s/g, 'Text Domain: text-domain');
+        result = result.replace(/_s/g, 'Dock_block');
+        result = result.replace(/_s-/g, 'prefix-handler');
+        result = result.replace(/_S_/g, 'CONST');
+        
+        fs.writeFile(file, result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
     });
-});
+}
+
+readdircontent(directoryPath);
